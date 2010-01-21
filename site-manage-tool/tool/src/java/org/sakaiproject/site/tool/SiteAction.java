ca8293948ddec8f48e600b47160758c386650536
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2010-01-21 15:10:07 +0000

    WL-1072 Limit action for admin sites.

--- site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -513,6 +513,9 @@ public class SiteAction extends PagedResourceActionII {
 	// list of UM Grad Tools site types for editing
 	public static final String GRADTOOLS_SITE_TYPES = "gradtools_site_types";
 
+	// list of Admin site types which should be restricted.
+	public static final String ADMIN_SITE_TYPES = "admin_site_types";
+
 	public static final String SITE_DUPLICATED = "site_duplicated";
 
 	public static final String SITE_DUPLICATED_NAME = "site_duplicated_named";
@@ -979,6 +982,16 @@ public class SiteAction extends PagedResourceActionII {
 								.getStrings("gradToolsSiteType")));
 			}
 			state.setAttribute(GRADTOOLS_SITE_TYPES, gradToolsSiteTypes);
+		}
+		
+		if (state.getAttribute(ADMIN_SITE_TYPES) == null) {
+			List<String> adminSiteTypes = Collections.emptyList();
+			if (ServerConfigurationService.getStrings("adminSiteType") != null) {
+				adminSiteTypes = new ArrayList<String>(Arrays
+						.asList(ServerConfigurationService
+								.getStrings("adminSiteType")));
+			}
+			state.setAttribute(ADMIN_SITE_TYPES, adminSiteTypes);
 		}
 		
 		if (ServerConfigurationService.getStrings("titleEditableSiteType") != null) {
@@ -1947,14 +1960,9 @@ public class SiteAction extends PagedResourceActionII {
 				if (allowUpdateSite) 
 				{
 					if (!isMyWorkspace) {
-						List gradToolsSiteTypes = (List) state
-								.getAttribute(GRADTOOLS_SITE_TYPES);
-						boolean isGradToolSite = false;
-						if (siteType != null
-								&& gradToolsSiteTypes.contains(siteType)) {
-							isGradToolSite = true;
-						}
-						if (!isGradToolSite) {
+
+						boolean isRestrictedSite = isRestrictedSite(state,siteType);
+						if (!isRestrictedSite) {
 							// hide site access for GRADTOOLS
 							// type of sites
 							b.add(new MenuEntry(
@@ -3161,6 +3169,27 @@ public class SiteAction extends PagedResourceActionII {
 		// should never be reached
 		return (String) getContext(data).get("template") + TEMPLATE[0];
 	}
+
+	/**
+	 * Some site don't get the full set of options in Site Info.
+	 * @param state The session state.
+	 * @param siteType The type of the site, eg project, course.
+	 * @return <code>true</code> is the site is a restricted one.
+	 */
+	private boolean isRestrictedSite(SessionState state, String siteType) {
+		List gradToolsSiteTypes = (List) state.getAttribute(GRADTOOLS_SITE_TYPES);
+		List adminSiteTypes = (List) state.getAttribute(ADMIN_SITE_TYPES);
+
+		boolean isRestrictedSite = false;
+		if (siteType != null && gradToolsSiteTypes.contains(siteType)) {
+			isRestrictedSite = true;
+		} else if (siteType != null && adminSiteTypes.contains(siteType) && !SecurityService.isSuperUser()) {
+			isRestrictedSite = true;
+		}
+		return isRestrictedSite;
+	}
+
+
 
 	private void addAccess(Context context, Map<String, AdditionalRole> access) {
 		boolean disableAdditional = access.size() == 0;
