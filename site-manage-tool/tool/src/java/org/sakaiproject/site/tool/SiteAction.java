8df7ea5da96c21d36d82b4852feb5514a5d5d9c6
Colin Tatham <colin.tatham@oucs.ox.ac.uk>
2010-08-10 10:43:47 +0100

    WL-1291 Allow increased CP upload file size limit

--- site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -2896,6 +2896,14 @@ public class SiteAction extends PagedResourceActionII {
 			 * buildContextForTemplate chef_siteInfo-importMtrlMaster.vm
 			 * 
 			 */
+			// want to allow a larger file upload size than system default, but within ceiling defined in properties:
+			String ceiling = ServerConfigurationService.getString("content.upload.ceiling");
+			if (!ceiling.equals(""))
+			{
+				long ceilingMb = Long.valueOf(ceiling);
+				context.put("uploadmax", ceilingMb);
+			}
+
 			return (String) getContext(data).get("template") + TEMPLATE[45];
 
 		case 46:
@@ -3565,6 +3573,8 @@ public class SiteAction extends PagedResourceActionII {
 		SessionState state = ((JetspeedRunData) data)
 				.getPortletSessionState(((JetspeedRunData) data).getJs_peid());
 
+		// removed upload file size checks; size check is done in RequestFilter, no need to repeat here.
+
 		List allzipList = new Vector();
 		List finalzipList = new Vector();
 		List directcopyList = new Vector();
@@ -3574,33 +3584,13 @@ public class SiteAction extends PagedResourceActionII {
 		String fileName = null;
 		fileFromUpload = data.getParameters().getFileItem("file");
 
-		String max_file_size_mb = ServerConfigurationService.getString(
-				"content.upload.max", "1");
-		int max_bytes = 1024 * 1024;
-		try {
-			max_bytes = Integer.parseInt(max_file_size_mb) * 1024 * 1024;
-		} catch (Exception e) {
-			// if unable to parse an integer from the value
-			// in the properties file, use 1 MB as a default
-			max_file_size_mb = "1";
-			max_bytes = 1024 * 1024;
-			M_log.warn(this + ".doUpload_Mtrl_Frm_File: wrong setting of content.upload.max = " + max_file_size_mb, e);
-		}
-		if (fileFromUpload == null) {
-			// "The user submitted a file to upload but it was too big!"
-			addAlert(state, rb.getString("importFile.size") + " "
-					+ max_file_size_mb + "MB "
-					+ rb.getString("importFile.exceeded"));
-		} else if (fileFromUpload.getFileName() == null
+		if (fileFromUpload.getFileName() == null
 				|| fileFromUpload.getFileName().length() == 0) {
 			addAlert(state, rb.getString("importFile.choosefile"));
 		} else {
 			byte[] fileData = fileFromUpload.get();
 
-			if (fileData.length >= max_bytes) {
-				addAlert(state, rb.getString("size") + " " + max_file_size_mb
-						+ "MB " + rb.getString("importFile.exceeded"));
-			} else if (fileData.length > 0) {
+			if (fileData.length > 0) {
 
 				if (importService.isValidArchive(fileData)) {
 					ImportDataSource importDataSource = importService
