7e009b4afa11e5c5ac443668c4ee34d21963d5af
Matthew Buckett <matthew.buckett@oucs.ox.ac.uk>
2012-04-16 15:57:47 +0100

    WL-1970 Keep site icon / site info url when duplicating.
    If the URLs were relative to the host the URL was lost (set to empty).
    Reported to Sakai under:
    https://jira.sakaiproject.org/browse/SAK-22065
    Conflicts:
    	site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java

--- site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
+++ site-manage-tool/tool/src/java/org/sakaiproject/site/tool/SiteAction.java
@@ -27,6 +27,11 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.text.NumberFormat;
+import java.lang.reflect.Method;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Calendar;
@@ -9803,13 +9808,22 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 	private String transferSiteResource(String oSiteId, String nSiteId, String siteAttribute) {
 		String rv = "";
 		
-		String accessUrl = ServerConfigurationService.getAccessUrl();
-		if (siteAttribute!= null && siteAttribute.indexOf(oSiteId) != -1 && accessUrl != null)
+		String access = ServerConfigurationService.getAccessUrl();
+		if (siteAttribute!= null && siteAttribute.indexOf(oSiteId) != -1 && access != null)
 		{
-			// stripe out the access url, get the relative form of "url"
-			Reference ref = EntityManager.newReference(siteAttribute.replaceAll(accessUrl, ""));
+			Reference ref = null;
 			try
 			{
+				URI accessUrl = new URI(access);
+				URI url = new URI(siteAttribute);
+				String path = url.getPath();
+				String accessPath = accessUrl.getPath();
+				
+				// stripe out the access url, get the relative form of "url"
+				String contentRef = path.replaceAll(accessPath, "");
+				
+				ref = EntityManager.newReference(contentRef);
+				
 				ContentResource resource = m_contentHostingService.getResource(ref.getId());
 				// the new resource
 				ContentResource nResource = null;
@@ -9834,6 +9848,10 @@ private Map<String,List> getTools(SessionState state, String type, Site site) {
 				// get the new resource url
 				rv = nResource != null?nResource.getUrl(false):"";
 				
+			}
+			catch (URISyntaxException use)
+			{
+				M_log.warn("Couldn't update site resource: "+ siteAttribute + " "+ use.getMessage());
 			}
 			catch (Exception refException)
 			{
