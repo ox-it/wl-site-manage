4c92a97bbde7e9f3003737669cfb4f6bdad280fa
Neelam Bhandari <neelam.bhandari@it.ox.ac.uk>
2015-07-15 11:16:37 +0100

    WL-3797 Change label of Joinable set to Joinable Group and add deletion confirmation
    changed indentation 'to tabs' in files 'JoinableGroupDelViewParameters.java'
    and 'JoinableGroupDelProducer.java'

--- site-manage-group-section-role-helper/tool/src/java/org/sakaiproject/site/tool/helper/managegroupsectionrole/rsf/JoinableGroupDelProducer.java
+++ site-manage-group-section-role-helper/tool/src/java/org/sakaiproject/site/tool/helper/managegroupsectionrole/rsf/JoinableGroupDelProducer.java
@@ -24,26 +24,26 @@ import uk.org.ponder.rsf.viewstate.ViewParamsReporter;
  */
 public class JoinableGroupDelProducer
 implements ViewComponentProducer, ActionResultInterceptor, ViewParamsReporter {
-    
+
 	/** Our log (commons). */
 	private static Log M_log = LogFactory.getLog(JoinableGroupDelProducer.class);
 	
-    public static final String VIEW_ID = "JoinableGroupDel";
-    public MessageLocator messageLocator;
-    public SiteManageGroupSectionRoleHandler handler;
-    public FrameAdjustingProducer frameAdjustingProducer;
-    
-    public String getViewID() {    
-        return VIEW_ID;
-    }
+	public static final String VIEW_ID = "JoinableGroupDel";
+	public MessageLocator messageLocator;
+	public SiteManageGroupSectionRoleHandler handler;
+	public FrameAdjustingProducer frameAdjustingProducer;
 
-    private TargettedMessageList tml;
+	public String getViewID() {
+		return VIEW_ID;
+	}
+
+	private TargettedMessageList tml;
 	public void setTargettedMessageList(TargettedMessageList tml) {
 		this.tml = tml;
 	}
-    public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker arg2) {
+	public void fillComponents(UIContainer tofill, ViewParameters viewparams, ComponentChecker arg2) {
 
-    	UIOutput.make(tofill, "page-title", messageLocator.getMessage("editgroup.removegroups"));
+		UIOutput.make(tofill, "page-title", messageLocator.getMessage("editgroup.removegroups"));
 		
 		UIForm deleteForm = UIForm.make(tofill, "delete-jg-confirm-form");
 		 
@@ -58,8 +58,8 @@ implements ViewComponentProducer, ActionResultInterceptor, ViewParamsReporter {
 		UIOutput.make(deleteForm, "prompt", messageLocator.getMessage("editgroup.removegroups"));
 		UILabelTargetDecorator.targetLabel(UIMessage.make(deleteForm, "group-title-group", "group.joinable.setname"), UIOutput.make(deleteForm, "groupTitle-group", joinableSetId));
 
-        if (delete) {
-            UIOutput.make(deleteForm, "current-groups-title", messageLocator.getMessage("group.joinable.currentgroups"));
+		if (delete) {
+			UIOutput.make(deleteForm, "current-groups-title", messageLocator.getMessage("group.joinable.currentgroups"));
 			int i = 0;
 			for(Group group : handler.site.getGroups()){
 				String joinableSet = group.getProperties().getProperty(group.GROUP_PROP_JOINABLE_SET);
@@ -72,27 +72,27 @@ implements ViewComponentProducer, ActionResultInterceptor, ViewParamsReporter {
 
 		}
 		UICommand.make(deleteForm, "delete-groups",  UIMessage.make("editgroup.removegroups"), "#{SiteManageGroupSectionRoleHandler.processDeleteJoinableSet}");
-        //Set the cancel button to redirect to EditJoinableSet form
-        UIForm deleteJoinableSetForm = UIForm.make(tofill, "delete-jg-form",new CreateJoinableGroupViewParameters(CreateJoinableGroupsProducer.VIEW_ID, joinableSetId));
+		//Set the cancel button to redirect to EditJoinableSet form
+		UIForm deleteJoinableSetForm = UIForm.make(tofill, "delete-jg-form",new CreateJoinableGroupViewParameters(CreateJoinableGroupsProducer.VIEW_ID, joinableSetId));
 		UICommand cancel = UICommand.make(deleteJoinableSetForm, "cancel", UIMessage.make("editgroup.cancel"));
 		cancel.parameters.add(new UIDeletionBinding("#{destroyScope.resultScope}"));
    
 		//process any messages
-        UIBranchContainer errorRow = UIBranchContainer.make(tofill,"error-row:", "0");
+		UIBranchContainer errorRow = UIBranchContainer.make(tofill,"error-row:", "0");
 		UIMessage.make(errorRow,"error","editgroup.groupdel.alert", new String[]{});
-    }
+	}
 
-    @Override
-    public ViewParameters getViewParameters() {
-        JoinableGroupDelViewParameters params = new JoinableGroupDelViewParameters();
-        params.id = null;
-        return params;
-    }
-    
-    // new hotness
-    public void interceptActionResult(ARIResult result, ViewParameters incoming, Object actionReturn) {
-        if ("success".equals(actionReturn) || "cancel".equals(actionReturn)) {
-            result.resultingView = new SimpleViewParameters(GroupListProducer.VIEW_ID);
-        }
-    }
+	@Override
+	public ViewParameters getViewParameters() {
+		JoinableGroupDelViewParameters params = new JoinableGroupDelViewParameters();
+		params.id = null;
+		return params;
+	}
+
+	// new hotness
+	public void interceptActionResult(ARIResult result, ViewParameters incoming, Object actionReturn) {
+		if ("success".equals(actionReturn) || "cancel".equals(actionReturn)) {
+			result.resultingView = new SimpleViewParameters(GroupListProducer.VIEW_ID);
+		}
+	}
 }
--- site-manage-group-section-role-helper/tool/src/java/org/sakaiproject/site/tool/helper/managegroupsectionrole/rsf/JoinableGroupDelViewParameters.java
+++ site-manage-group-section-role-helper/tool/src/java/org/sakaiproject/site/tool/helper/managegroupsectionrole/rsf/JoinableGroupDelViewParameters.java
@@ -5,12 +5,12 @@ import uk.org.ponder.rsf.viewstate.SimpleViewParameters;
 public class JoinableGroupDelViewParameters extends SimpleViewParameters{
 	public String id;
 
-    public JoinableGroupDelViewParameters(String id) {
+	public JoinableGroupDelViewParameters(String id) {
 		super();
 		this.id = id;
 	}
 
-    public JoinableGroupDelViewParameters() {
+	public JoinableGroupDelViewParameters() {
 		super();
 	}
 
@@ -18,11 +18,11 @@ public class JoinableGroupDelViewParameters extends SimpleViewParameters{
 		this.id= id;
 		this.viewID = viewId;
 	}
-    
-    public String getId()
-    {
-    	return id;
-    }
+
+	public String getId()
+	{
+		return id;
+	}
 
 	public void setId(String id) {
 		this.id = id;
