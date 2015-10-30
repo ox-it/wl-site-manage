f57e3848150e24b630018181ee5160b0915e25ef
Surya Govindarajan <surya.govindarajan@it.ox.ac.uk>
2015-10-30 10:19:50 +0000

    WL-3887 Adding duplicate site variables to context,displaying in flash notification UI

--- site-manage-tool/tool/src/webapp/js/site-manage.js
+++ site-manage-tool/tool/src/webapp/js/site-manage.js
@@ -823,19 +823,7 @@ var setupCategTools = function(){
 };
 
 var setupRecentSite = function(){
-
-    var target = $('#newSiteAlert').attr('class');
-
-    if (sessionStorage.getItem(target)) {
-        $('#newSiteAlert').hide();
-    }
-  
-    $('.newSiteAlertClose').click(function(e){
-        e.preventDefault();
-        $(this).closest('div').fadeOut('slow');
-        sessionStorage.setItem(target, true);
-    })
-
+    var target = $('#newSiteAlertPublish').attr('class');
     $('#newSiteAlertPublish').click(function(e){
         e.preventDefault();
         var reqUrl = '/direct/site/' + target + '/edit';
